a4e081459abef1e1145a8d847dfe14af5d4acc02
Ramsey Bland <ramsey.bland@gmail.com>
2018-07-01 12:45:21 -0600

    created favs and tags in db

--- DBTables.sql
+++ DBTables.sql
@@ -30,3 +30,31 @@
     --     REFERENCES posts(id)
     -- )
 
+-- CREATE TABLE userfavs (
+--   id int NOT NULL AUTO_INCREMENT,
+--   postId int NOT NULL,
+--   userId VARCHAR(255) NOT NULL,
+
+--   PRIMARY KEY (id),
+--   INDEX (userid),
+
+--   FOREIGN KEY (postId)
+--     REFERENCES posts(id)
+--     ON DELETE CASCADE,
+
+--   FOREIGN KEY (userId)
+--     REFERENCES users(id)
+--     ON DELETE CASCADE
+-- );
+
+  -- CREATE TABLE tags (
+  --   tag VARCHAR(45) NOT NULL,
+  --   postId int NOT NULL,
+
+  --   PRIMARY KEY (tag),
+  --   INDEX (postId),
+
+  --   FOREIGN KEY (postId)
+  --     REFERENCES posts(id)
+  -- )
+
